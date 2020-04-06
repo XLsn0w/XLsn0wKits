b60f3f25ac3cbc1d65b521f3374e894fcf563e78
xlsn0w <xlsn0wios@gmail.com>
2020-04-06 14:56:08 +0800

    add

--- Example/Pods/Target Support Files/PodLib/PodLib-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PodLib : NSObject
-@end
-@implementation PodsDummy_PodLib
-@end
--- Example/Pods/Target Support Files/Pods-PodLib_Example/Pods-PodLib_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PodLib_Example : NSObject
-@end
-@implementation PodsDummy_Pods_PodLib_Example
-@end
